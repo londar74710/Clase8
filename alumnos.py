028534d8e2e2287d1b2513c289815c3a6b97fa97
Darwin Portela <darwin74710@gmail.com>
2023-10-24 22:22:21 -0500

    Muestra los alumnos que perdieron la materia

--- alumnos.py
+++ alumnos.py
@@ -34,3 +34,11 @@ def alumnos():
             promedio = (alumno[1]+alumno[2]+alumno[3])/3
             #el round es para redondear y el 1 es para decir cuantos numeros quiere redondear.
             print(alumno[0]+"\t" +str(round(promedio,1)))
+
+    if len(alumnos) > 0:
+        print("\nListado de los alumnos que perdieron la materia")
+        print("Nombre\tPromedio")
+        for alumno in alumnos:
+            promedio = (alumno[1]+alumno[2]+alumno[3])/3
+            if promedio < 3:
+                print(alumno[0]+"\t" +str(round(promedio,1)))
