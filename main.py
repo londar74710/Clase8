56dde654247934034d8526f4547f61d0c6566621
unknown <vargas.j0613@gmail.com>
2023-11-02 13:02:47 -0500

    Se agrega el archivo alumnos

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
 import problema1
 from alumnos import alumnos
+'''
+hasta ahora hemos
+trabajado con variables
+que permiten almacenar
+ un unico valor
+ '''
 
 edad = 12
-altura = 1.75
-nombre = "Juan"
+
+altura = 1.79
+
+nombre = "juan"
+
 estado = True
 
-lista1 = [10, 2, 3, 4]
-lista2 = [1.72, 1.54, 1.23, 1.64]
+print("hello world")
+
+'''
+En python podemos 
+usar una variable
+que alamcena  una
+coleccion de datos 
+y luego accederla usando
+ un subindice'''
+
+#lista de enteros
+lista1 = [10, 5, 3, 9]
+
+#lista de decimales
+lista2 = [1.78, 2.66, 1.55, 89.4]
+
+#lista de string
 lista3 = ["lunes", "martes", "miercoles"]
+
+#lista elementos de distinto tipo
 lista4 = ["juan", 45, 1.92, False]
 
 
-
-
 if __name__ == '__main__':
-    # Mostrar cantidad de elementos en las listas
+
     print(len(lista1))
     print(len(lista2))
     print(len(lista3))
     print(len(lista4))
 
-    #Establecer un espacio
     print()
-    # Mostar elementos de la lista
-    print(lista1)
-    print()
-    # Cambiar valores de listas
-    lista1[0] = 1
-    print(lista1)
-
-    print()
-    # Mostrar un valor exacto de lista, siempre inicia en 0 el primer elemento.
-    print(lista1[3])
+    print(lista1, lista2, lista3, lista4)
 
     print()
 
     problema1.sumar_5_enteros()
+    
+    print()
+
+    alumnos()
+
